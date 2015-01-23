3ac6df1a5c56d470a79d9e4edc5beb4672e927d3
yyj20934 <yyj20934@naver.com>
2015-01-23 15:05:29 +0900

    되나??

--- easyjava/src/easyjava/SimpleTest1.java
+++ easyjava/src/easyjava/SimpleTest1.java
@@ -4,7 +4,7 @@ public class SimpleTest1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// 프린트 테스트ㄴ
+		// 프린트 테스트ㄴ 
 		for(int i=2;i<10;i++) {
 			System.out.println(i);
 		}
