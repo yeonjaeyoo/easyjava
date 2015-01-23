f2e4295e46a87e6b88565679bb811bc501d929d6
yyj20934 <yyj20934@naver.com>
2015-01-23 14:55:47 +0900

    되라

---
+++ easyjava/src/easyjava/SimpleTest1.java
@@ -0,0 +1,13 @@
+package easyjava;
+
+public class SimpleTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 프린트 테스트ㄴ
+		for(int i=2;i<10;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
