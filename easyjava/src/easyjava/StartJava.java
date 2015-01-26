2a3f0e70fc057f350eaa9994284338e02ab1e606
yyj20934 <yyj20934@naver.com>
2015-01-26 14:36:05 +0900

    인코딩 변경

--- easyjava/src/easyjava/StartJava.java
+++ easyjava/src/easyjava/StartJava.java
@@ -4,7 +4,7 @@ public class StartJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ÀÚ¹ÙÀÇ ½ÃÀÛÀÔ´Ï´Ù.");
+		System.out.println("ì¸ì½”ë”© ë³€ê²½.");
 	}
 
 }
