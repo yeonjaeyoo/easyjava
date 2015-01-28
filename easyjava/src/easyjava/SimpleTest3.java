c37d2197a60222fcdc9543fad90a981fe9539008
yyj20934 <yyj20934@naver.com>
2015-01-28 15:17:49 +0900

    int, double 테스트

---
+++ easyjava/src/easyjava/SimpleTest3.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package easyjava;
+
+/**
+ * @author educar
+ *
+ */
+public class SimpleTest3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int iNum1 = 5, iNum2;
+		double dNum1, dNum2;
+		
+		iNum2 = 10;
+		dNum1 = 5.0;
+		dNum2 = 10.0;
+		
+		System.out.println(iNum1 + " * " + iNum2 + " = " + iNum1 * iNum2);
+		System.out.println(dNum1 + " * " + dNum2 + " = " + dNum1 * dNum2);
+	}
+
+}
