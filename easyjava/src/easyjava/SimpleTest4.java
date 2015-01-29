099540e7c624258a7b037d11522bce88ad3c0ae3
yyj20934 <yyj20934@naver.com>
2015-01-29 14:12:15 +0900

    String 예제

---
+++ easyjava/src/easyjava/SimpleTest4.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package easyjava;
+
+/**
+ * @author educar
+ *
+ */
+public class SimpleTest4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1, str2, str3;
+		str1 = "I'm";
+		str2 = " a ";
+		str3 = "beginner.";
+		
+		System.out.println(str1 + str2 + str3);
+	}
+}
