c0549af48358a1e86ab99d94eac5c83053eb5400
yyj20934 <yyj20934@naver.com>
2015-02-03 11:18:17 +0900

    while 예제

---
+++ easyjava/src/easyjava/SimpleTest8.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package easyjava;
+
+/**
+ * @author educar
+ *
+ */
+public class SimpleTest8 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int iNum1;
+		iNum1 = 0;
+		
+		while(iNum1 < 10) {
+			iNum1++;
+			System.out.println("iNum1 값 = " + iNum1);
+		}
+	}
+
+}
