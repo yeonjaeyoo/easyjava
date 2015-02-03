4611da4d2c70390c3918ba37b05ee8becf1b73bf
yyj20934 <yyj20934@naver.com>
2015-02-03 13:40:17 +0900

    do while 문 예제

---
+++ easyjava/src/easyjava/SimpleTest9.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package easyjava;
+
+/**
+ * @author educar
+ *
+ */
+public class SimpleTest9 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int iNum1;
+		iNum1 = 0;
+		
+		do {
+			iNum1++;
+			System.out.println("iNum1 값 = " + iNum1);
+		} while(iNum1 < 10);
+	}
+
+}
