148af9a31d0ab4523df4f7d431eec0f26067df0c
yyj20934 <yyj20934@naver.com>
2015-02-06 15:32:53 +0900

    증감연산자 예제

---
+++ easyjava/src/easyjava/SimpleTest11.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package easyjava;
+
+/**
+ * @author educar
+ *
+ */
+public class SimpleTest11 {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 5, j = 5;
+		System.out.println("첫번째i = "+ ++i);
+		System.out.println("첫번째j = "+ j++);
+		
+		System.out.println("두번째i = "+ i);
+		System.out.println("두번째j = "+ j);
+	}
+
+}
