a2273005a86ffb5ba9142738e27237c62dc2577f
yyj20934 <yyj20934@naver.com>
2015-03-26 12:55:04 +0900

    커밋

--- easyjava/src/easyjava/SimpleTest15.java
+++ easyjava/src/easyjava/SimpleTest15.java
@@ -13,12 +13,56 @@ public class SimpleTest15 {
 	 */
 	public static void main(String[] args) {
 		String sNum;
-		sNum = "-10.3";
-		
+		sNum = "-10.23";
+		System.out.println("정수="+String.format("%.1f", new Double(-10.23)));
 		String[] sSplitNum = sNum.split("[.]");
 		System.out.println("정수="+sSplitNum[0]);
 		System.out.println("정수="+sSplitNum[0].substring(0, 1));
 		System.out.println("정수="+sSplitNum[0].substring(1));
 		System.out.println("소수="+sSplitNum[1]);
+		
+		/*
+		This task could be completed by the pattern that can be visualized between the blocks, similar to recalling phone numbers from a visualized dial plate.
+		이 과업은  시각화된 다이얼 판에서 전화 번호를 상기시킴과 비슷한 블록 사이에서 시각화 할 수 있는 패턴에 의해 작성할 수 있다.
+
+		Using this strategy, the pattern between the blocks is recalled.
+		이 전략을 이용해서, 블록 사이의 패턴은 상기된다.
+
+		Thus, Corsi block tasks are not comparable to tasks recalling digit sequences because it is not mandatory to remember different elements from a sequential store step by step as required for digit sequences.
+		이 자리 시퀀스에 필요한 단계별로 순차적으로 저장하는 단계에서 서로 다른 요소를 기억하는 것이 필수가 아니기 때문에 따라서, Corsi은 블록 작업은 자리 시퀀스를 리콜 작업에 비교할 수 없습니다.
+
+		Gras-Vincendon et al. (1994) applied classic Corsi blocks to tasks that had to be reproduced by tapping on sketched cubes.
+		Gras-Vincendon et al. (1994)은 스케치 큐브로 테이핑하여 재생된 작업에 고전 Corsi은 블록을 적용했다.
+
+		The results confirmed group differences between 8 children with severe SLI compared to a control group matched by age and performance IQ, although the classification criteria of SLI were lacking.
+		SLI의 분류 기준이 부족 되었더라도 그 결과, 나이 및 성능 IQ의 일치 대조군에 비해 심각한 SLI를 가진 8아이 그룹 간의 차이를 확인 하였다.
+
+		Hoffman and Gillam (2004) used more complex tasks that tap working memory:
+		Hoffman and Gillam (2004)은 작업 기억을 누르는 보다 복잡한 작업을 사용합니다:
+
+		Visually presented digits had to be recalled verbally, and sequences of locations on a grid had to be pointed out.
+		시각적으로 제시된 숫자는 구두로 리콜했고, 그리드에 위치 순서는 지적했다.
+
+		Each task was performed with or without an additional exercise, such as naming or pointing out the color of the last digit or the last location.
+		각 작업은 부가적인 운동이 있거나 없으며, 마지막 자릿수 혹은 마지막 위치의 색깔을 지적하거나 명명하는 거 같은 것으로 수행된다.
+
+		In comparison to the typically developing control group, children with SLI (n = 24) showed a generally poorer recall of digits and locations for all conditions.
+		그 전형적으로 발달 제어 그룹과 비교해 보면, SLI(n=24)를 가진 아이들은 자리와 위치의 모든 조건에 대한 일반적으로 형편없는 리콜을 보여 주었다.
+
+		The authors concluded that storage capacities for both verbal and visual information in children with SLI were smaller than in the group of normal children.
+		그 저자들은 SLI을 가진 아이들이 언어와 시각 정보의 저장 용량은 정상적인 아이들의 그룹에 비교하여 작다고 결론을 내렸다.
+
+		However, in their tasks, several colors were used, providing opportunities for verbal recoding by color matching to identify locations, as discussed by Archibald and Gathercole (2006b).
+		그러나, 그들의 작업에, 여러 가지 색상은 Archibald와 Gathercole(2006b)에 의해 논의 된 바와 같이, 위치를 식별하는 컬러 매칭에 의해 구두 남길 기회를 제공 하였다.
+
+		Furthermore, the study population included children with R/ELI and ELI, but the results of both language groups were not analyzed separately.
+		또한, 연구 대상은 R/ELI과 ELI 어린이를 포함하지만, 두 언어 그룹의 결과를 개별적으로 분석되지 않았다.
+
+		Also in the aforementioned studies, the study populations consisted of mixed groups of children with ELI and R/ELI, but neither presented subgroup analyses by R/ELI or ELI (Bavin et al., 2005; Gillam et al., 1998) or a clear SLI classification (Gras-Vincendon et al., 1994). 
+		또한, 상기한 연구에서, 연구 집단은 ELI 및 R/ELI 아동의 혼합 그룹으로 구성되어 있지만, 제시된 하위 그룹은 R/ELI 또는 ELI 또는 명확한 SLI 분류하지 않는다.
+
+		Therefore, the observed group mean differences might reflect differences in subpopulations with either R/ELI or ELI.
+		따라서, 관찰된 그룹 차이 집단에서 R/ELI 또는 ELI과 서로의 다른 점을 반영할 수 있는 것을 의미한다.
+		*/
 	}
 }
