f30d5cfd40a49091adc9342d0efd9b4b66fc5bfe
yyj20934 <yyj20934@naver.com>
2015-04-14 12:39:31 +0900

    static 예제 1

---
+++ easyjava/src/easyjava/SimpleTest16.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package easyjava;
+
+/**
+ * @author educar
+ *
+ */
+public class SimpleTest16 {
+	private static int i = 0;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("i="+i);
+		i++;
+		System.out.println("i="+i);
+		i++;
+		System.out.println("i="+SimpleTest16.i);
+	}
+}
