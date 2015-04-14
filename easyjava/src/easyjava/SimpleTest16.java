1f293826442921b6d1f2c6d76998a2b83041588e
yyj20934 <yyj20934@naver.com>
2015-04-14 12:50:56 +0900

    static 예제2

--- easyjava/src/easyjava/SimpleTest16.java
+++ easyjava/src/easyjava/SimpleTest16.java
@@ -9,14 +9,28 @@ package easyjava;
  */
 public class SimpleTest16 {
 	private static int i = 0;
+	private int j = 0;
+	
+	public SimpleTest16() {
+		++i;
+		++j;
+	}
+	
+	public void printNumber() {
+		System.out.println("i="+i);
+		System.out.println("j="+j);
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("i="+i);
-		i++;
-		System.out.println("i="+i);
-		i++;
-		System.out.println("i="+SimpleTest16.i);
+		System.out.println("첫번째 호출");
+		SimpleTest16 st1 = new SimpleTest16();
+		st1.printNumber();
+		
+		System.out.println("두번째 호출");
+		SimpleTest16 st2 = new SimpleTest16();
+		st2.printNumber();
 	}
 }
