3819af3894122dd95ca755241e49b5b2d850417f
yyj20934 <yyj20934@naver.com>
2015-05-08 09:35:32 +0900

    break 예제

---
+++ easyjava/src/easyjava/SimpleTest26.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package easyjava;
+
+/**
+ * @author educar
+ *
+ */
+public class SimpleTest26 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<10; i++) {
+			System.out.println("i="+i);
+			
+			if(i==6) 
+				break;
+		}
+	}
+
+}
