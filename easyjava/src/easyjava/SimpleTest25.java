f77e036ec1173dc8c0ba3baeb025f8cf3d4691e1
yyj20934 <yyj20934@naver.com>
2015-05-08 09:35:03 +0900

    continue 예제

---
+++ easyjava/src/easyjava/SimpleTest25.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package easyjava;
+
+/**
+ * @author educar
+ *
+ */
+public class SimpleTest25 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<10; i++) {
+			if(i==6) 
+				continue;
+			
+			System.out.println("i="+i);
+		}
+	}
+
+}
