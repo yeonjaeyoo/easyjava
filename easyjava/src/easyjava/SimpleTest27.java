5342a4dc85c5e624d2e6fb01010484a1a35eb4a6
yyj20934 <yyj20934@naver.com>
2015-05-09 11:12:50 +0900

    boolean 예제

---
+++ easyjava/src/easyjava/SimpleTest27.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package easyjava;
+
+/**
+ * @author educar
+ *
+ */
+public class SimpleTest27 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean isResult1;
+		boolean isResult2;
+		
+		int i, j;
+		i = 10;
+		j = 9;
+		
+		isResult1 = i > j;
+		isResult2 = i < j;
+		
+		System.out.println("isResult1 = " + isResult1);
+		System.out.println("isResult2 = " + isResult2);
+	}
+}
