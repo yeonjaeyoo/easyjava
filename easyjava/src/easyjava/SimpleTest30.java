21de8edc34f361e0df49b73ed8493857c4e145c3
yyj20934 <yyj20934@naver.com>
2015-05-20 12:55:46 +0900

    for 조건문이 항상 참인경우(무한루프)

---
+++ easyjava/src/easyjava/SimpleTest30.java
@@ -0,0 +1,14 @@
+package easyjava;
+
+public class SimpleTest30 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("for문이 실행될까요?");
+		
+		for(int i=10;i>9;i++) {
+			System.out.println("i="+i);
+		}
+	}
+
+}
