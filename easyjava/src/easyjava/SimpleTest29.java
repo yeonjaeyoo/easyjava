db6903ff8041c18f8755fa4667bc2c45daad90ce
yyj20934 <yyj20934@naver.com>
2015-05-20 12:54:08 +0900

    for 조건이 일치안될 때(실행안됨)

---
+++ easyjava/src/easyjava/SimpleTest29.java
@@ -0,0 +1,15 @@
+package easyjava;
+
+public class SimpleTest29 {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("for문이 실행될까요?");
+		
+		for(int i=10;i>10;i++) {
+			System.out.println("i="+i);
+		}
+	}
+
+}
