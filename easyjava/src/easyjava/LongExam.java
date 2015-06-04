2de16864b583cf9ce54afbc0c17a074daec70fc4
yyj20934 <yyj20934@naver.com>
2015-06-04 13:03:54 +0900

    long 예제

---
+++ easyjava/src/easyjava/LongExam.java
@@ -0,0 +1,12 @@
+package easyjava;
+
+public class LongExam {
+
+	public static void main(String[] args) {
+		long i = 10000L;
+		long j = -10000L;
+		
+		System.out.println("i = "+ i);
+		System.out.println("j = "+ j);
+	}
+}
