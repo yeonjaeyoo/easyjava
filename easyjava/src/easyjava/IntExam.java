e67299e62f13e7d4a3d20165c4d89419d162b6aa
yyj20934 <yyj20934@naver.com>
2015-06-04 12:48:54 +0900

    int 예제

---
+++ easyjava/src/easyjava/IntExam.java
@@ -0,0 +1,13 @@
+package easyjava;
+
+public class IntExam {
+
+	public static void main(String[] args) {
+		int i = 1;
+		int j = -1;
+		
+		System.out.println("i = "+ i);
+		System.out.println("j = "+ j);
+	}
+
+}
