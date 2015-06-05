557fd213eb9aa47dacb53a3c92ba3b8248ceeadf
yyj20934 <yyj20934@naver.com>
2015-06-05 12:51:13 +0900

    double 예제

---
+++ easyjava/src/easyjava/DoubleExam.java
@@ -0,0 +1,11 @@
+package easyjava;
+
+public class DoubleExam {
+	public static void main(String[] args) {
+		double i = 10.5;
+		System.out.println("i = "+ i);
+		
+		i = 0.333;
+		System.out.println("i = "+ i);
+	}
+}
