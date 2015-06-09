708a46f6adee68ecdd3b4d5f9ae2e522ffd70862
yyj20934 <yyj20934@naver.com>
2015-06-09 12:43:54 +0900

    연산자 +=, -= 예제

---
+++ easyjava/src/easyjava/OperatorExam1.java
@@ -0,0 +1,12 @@
+package easyjava;
+
+public class OperatorExam1 {
+	public static void main(String[] args) {
+		int i = 5, j = 3;
+		
+		i += 5;
+		j -= 2;
+				
+		System.out.println("i = "+ i + ", j = " + j);
+	}
+}
