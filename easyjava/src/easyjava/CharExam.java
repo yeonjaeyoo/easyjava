38be4e1e60ad949825005459b8c276ff0087a30c
yyj20934 <yyj20934@naver.com>
2015-06-09 12:51:17 +0900

    char 예제

---
+++ easyjava/src/easyjava/CharExam.java
@@ -0,0 +1,13 @@
+package easyjava;
+
+public class CharExam {
+
+	public static void main(String[] args) {
+		char c = 'I';
+		System.out.println("char는 ''에서 사용해야 한다.");
+		System.out.println("c = " + c);
+		
+		//char는 ''에서 사용해야 한다.
+		//c = I
+	}
+}
