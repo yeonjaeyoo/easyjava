0aa3e888bb1803efece004fbee9fcbbe0fd1a1b8
yyj20934 <yyj20934@naver.com>
2015-06-09 12:48:03 +0900

    boolean 예제

---
+++ easyjava/src/easyjava/BooleanExam.java
@@ -0,0 +1,14 @@
+package easyjava;
+
+public class BooleanExam {
+
+	public static void main(String[] args) {
+		boolean i = false, j = true;
+		
+		System.out.println("i = " + i);
+		System.out.println("j = " + j);
+		
+		//i = false
+		//j = true
+	}
+}
