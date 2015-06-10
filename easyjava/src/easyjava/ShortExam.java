bbee6e49691617865b192d0b5316a24990abcc46
yyj20934 <yyj20934@naver.com>
2015-06-10 12:57:20 +0900

    short 예제

---
+++ easyjava/src/easyjava/ShortExam.java
@@ -0,0 +1,15 @@
+package easyjava;
+
+public class ShortExam {
+
+	public static void main(String[] args) {
+		short s = 3;
+		short k = -3;
+		
+		System.out.println("s = "+ s);
+		System.out.println("k = "+ k);
+		
+		//s = 3
+		//k = -3
+	}
+}
