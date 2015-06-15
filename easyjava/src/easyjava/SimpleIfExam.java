0bbdc6dfbf8b34e1b41658609c1537dc6ae2611f
yyj20934 <yyj20934@naver.com>
2015-06-15 12:35:13 +0900

    if 문 테스트

---
+++ easyjava/src/easyjava/SimpleIfExam.java
@@ -0,0 +1,22 @@
+package easyjava;
+
+public class SimpleIfExam {
+	public static void main(String[] args) {
+		int i = 5;
+		
+		System.out.println("if 문 테스트 시작");
+		
+		if(i > 4)
+			System.out.println("if 조건식 참입니다.");
+		
+		if(i > 5)
+			System.out.println("이것도 참인가요?");
+				
+		
+		System.out.println("if 문 테스트 끝");
+		
+		//if 문 테스트 시작
+		//if 조건식 참입니다.
+		//if 문 테스트 끝
+	}
+}	
