43c3fbc45c8e2c086ff4e1b33f9c16e29f007ac9
yyj20934 <yyj20934@naver.com>
2015-08-10 13:02:49 +0900

    insert 트랜잭션 처리

---
+++ easyjava/src/easyjava/DBConTest12.java
@@ -0,0 +1,95 @@
+package easyjava;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBConTest12 {
+	public static void main(String[] args) throws SQLException {
+		Connection conn = null;
+		Statement stmt = null;
+		Statement stmt2 = null;
+       
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/yyj?useUnicode=yes&amp;characterEncoding=UTF-8", "root", "root1234");
+			
+			System.out.println("mariadb 연결 성공");
+			
+			conn.setAutoCommit(false);
+			
+			int cnt;
+			String sId = "abcd2";
+			String sName = "홍길동2";
+			
+			String sql = "insert into member values ('" + sId + "', '" + sName + "')" ;
+			stmt = conn.createStatement();
+			cnt = stmt.executeUpdate(sql);
+			
+			System.out.println("결과="+ cnt);
+			
+			int cnt2;
+			String sId2 = "yyj20934";
+			String sName2 = "테스트10";
+			
+			String sql2 = "insert into member values ('" + sId2 + "', '" + sName2 + "')" ;
+			stmt2 = conn.createStatement();
+			cnt2 = stmt2.executeUpdate(sql2);
+			
+			System.out.println("결과2="+ cnt2);
+			
+			conn.commit();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+        } catch (SQLException e) {
+        	conn.rollback();
+        	e.printStackTrace();
+		} catch (Exception ex) {
+        	ex.printStackTrace();
+		} finally {
+			try {
+				if (stmt2 != null)
+					stmt2.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+			
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+			
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		System.out.println("mariadb 연결 종료");
+		/*
+		mariadb 연결 성공
+		결과=1
+		java.sql.SQLIntegrityConstraintViolationException: Duplicate entry 'yyj20934' for key 'PRIMARY'
+			at org.mariadb.jdbc.internal.SQLExceptionMapper.get(SQLExceptionMapper.java:132)
+			at org.mariadb.jdbc.internal.SQLExceptionMapper.throwException(SQLExceptionMapper.java:106)
+			at org.mariadb.jdbc.MySQLStatement.executeQueryEpilog(MySQLStatement.java:268)
+			at org.mariadb.jdbc.MySQLStatement.execute(MySQLStatement.java:296)
+			at org.mariadb.jdbc.MySQLStatement.executeUpdate(MySQLStatement.java:325)
+			at org.mariadb.jdbc.MySQLStatement.executeUpdate(MySQLStatement.java:357)
+			at easyjava.DBConTest12.main(DBConTest12.java:38)
+		Caused by: org.mariadb.jdbc.internal.common.QueryException: Duplicate entry 'yyj20934' for key 'PRIMARY'
+			at org.mariadb.jdbc.internal.mysql.MySQLProtocol.getResult(MySQLProtocol.java:982)
+			at org.mariadb.jdbc.internal.mysql.MySQLProtocol.executeQuery(MySQLProtocol.java:1042)
+			at org.mariadb.jdbc.MySQLStatement.execute(MySQLStatement.java:289)
+			... 3 more
+		mariadb 연결 종료
+
+		*/
+
+	}
+}
