6435d9cfe3013ef1572ce0d8c568dd662c7fad10
yyj20934 <yyj20934@naver.com>
2015-08-24 12:35:44 +0900

    String 예제1

---
+++ easyjava/src/easyjava/StringExam.java
@@ -0,0 +1,13 @@
+package easyjava;
+
+public class StringExam {
+
+	public static void main(String[] args) {
+		String sStr = "Java Programmer";
+		
+		int iLen = sStr.length();
+		
+		System.out.println("길이 = "+ iLen);
+		//길이 = 15
+	}
+}
